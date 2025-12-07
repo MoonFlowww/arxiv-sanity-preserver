a0edbf8e9e9df33f32531c109516315d8370850e
MoonFlowww <161968556+MoonFlowww@users.noreply.github.com>
2025-12-07 20:43:04 +0100

    Update thumb_pdf.py

--- thumb_pdf.py
+++ thumb_pdf.py
@@ -4,6 +4,7 @@ requires: sudo apt-get install imagemagick
 """
 
 import os
+import sys
 import time
 import shutil
 from subprocess import Popen
@@ -60,7 +61,13 @@ for i,p in enumerate(pdf_files):
 
   # spawn async. convert can unfortunately enter an infinite loop, have to handle this.
   # this command will generate 8 independent images thumb-0.png ... thumb-7.png of the thumbnails
-  pp = Popen(['convert', '%s[0-7]' % (pdf_path, ), '-thumbnail', 'x156', os.path.join(Config.tmp_dir, 'thumb.png')])
+  pp = Popen([
+    'convert',
+    '%s[0-7]' % (pdf_path, ),
+    '-thumbnail',
+    'x156',
+    os.path.join(Config.tmp_dir, 'thumb-%d.png')
+  ])
   t0 = time.time()
   while time.time() - t0 < 20: # give it 15 seconds deadline
     ret = pp.poll()
